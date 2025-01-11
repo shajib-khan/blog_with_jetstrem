ed78b0be074ee83c8699ca8d966d204ff7ea6035
shajib-khan <shajibkhan946@gmail.com>
2025-01-11 20:37:24 +0600

    jetstream bug fixed

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,18 @@
 use App\Http\Controllers\HomeController;
 use Illuminate\Support\Facades\Route;
 
+Route::get('/', function () {
+    return view('welcome');
+});
 
+//Route::get('/',[HomeController::class,'index'])->name('home');
 
-Route::get('/',[HomeController::class,'index'])->name('home');
+Route::middleware([
+    'auth:sanctum',
+    config('jetstream.auth_session'),
+    'verified',
+])->group(function () {
+    Route::get('/dashboard', function () {
+        return view('dashboard');
+    })->name('dashboard');
+});
